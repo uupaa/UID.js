f9f135df5d8bb50d50f0dec84edc3fc0215bf22f
uupaa <uupaa.js@gmail.com>
2014-01-21 04:07:47 +0900

    first revision

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 UID.js
-======
+=========
 
-Create Unique ID
+Create Unique ID.
+
+# API Document
+
+https://github.com/uupaa/UID.js/wiki/UID
+
+# Install, Setup modules
+
+```sh
+$ git clone git@github.com:uupaa/UID.js.git
+$ cd UID.js
+$ npm install
+```
+
+# Test
+
+```sh
+$ npm test
+```
---
+++ test/test.js
@@ -0,0 +1,25 @@
+new Test().add([
+        testUID
+    ]).run();
+
+function testUID(next) {
+
+    var uid1 = UID();
+    var uid2 = UID();
+    var uid3 = UID("a");
+    var uid4 = UID("a");
+    var uid5 = UID("b");
+    var uid6 = UID("b");
+
+
+    if (uid1 + 1 === uid2 &&
+        uid3 + 1 === uid4 &&
+        uid5 + 1 === uid6) {
+        console.log("testUID ok");
+        next && next.pass();
+    } else {
+        console.log("testUID ng");
+        next && next.miss();
+    }
+}
+
