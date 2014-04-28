e49bec3c50adc35303862567290deb57098697d1
uupaa <uupaa.js@gmail.com>
2014-04-28 20:47:17 +0900

    follow NodeModule.js

--- test/index.node.js
+++ test/index.node.js
@@ -1,7 +1,7 @@
-require("uupaa.valid.js");
-require("uupaa.task.js");
-require("uupaa.test.js");
+require("../node_modules/uupaa.nodemodule.js/lib/NodeModule.js");
+require("../node_modules/uupaa.valid.js/lib/Valid.js");
+require("../node_modules/uupaa.task.js/lib/Task.js");
+require("../node_modules/uupaa.test.js/lib/Test.js");
 require("../lib/UID.js");
 require("../release/UID.min.js");
 require("./test.js");
-
